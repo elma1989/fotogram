6c122fdf53a138d2b1f0320a725b47ef8b3c3905
Marco Elste <marco.elste@web.de>
2025-06-29 12:30:11 +0200

    feat: Updated app.js: toggoleOverlay()

--- static/app.js
+++ static/app.js
@@ -1,26 +1,35 @@
 import { renderSingleMiniPic } from "./template.js";
 
-let currentPictureIndex = 0;
-let maxPicture = 0;
+const refOverlay = document.querySelector('.overlay');
 
 async function renderFotos() {
     const refContent = document.querySelector('.fotoarea');
     const response = await fetch('static/picdata.json');
     const picdata = await response.json();
     refContent.innerHTML = "";
-    maxPicture = picdata.pics.length
-    for (let i = 0; i < maxPicture; i++) {
+    for (let i = 0; i < picdata.pics.length; i++) {
         refContent.innerHTML += renderSingleMiniPic(picdata.path + picdata.pics[i].file, picdata.pics[i].alt, i+1);
     }
     const fotos = document.querySelectorAll('.img-wrapper img');
     fotos.forEach(
         foto => {
             foto.addEventListener('click', e => {
-                currentPictureIndex = Number(String(e.currentTarget.getAttribute('id')).slice(3)) - 1;
-                console.log(currentPictureIndex);
+                renderFotoCard(Number(String(e.currentTarget.getAttribute('id')).slice(3)) - 1);
             });
         }
     );
 }
 
+async function renderFotoCard(index) {
+    toggleOverlay();
+}
+
+
+function toggleOverlay() {
+    console.log(document.querySelector('.overlay').classList.toggle('d-none'));
+}
+
 renderFotos();
+refOverlay.addEventListener('click', () => {
+    toggleOverlay();
+});
